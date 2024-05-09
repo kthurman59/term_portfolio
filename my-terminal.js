996cf96a701f9d48d024de97d879cba49403bcd2
kevin <kthurman59@gmail.com>
2024-05-09 13:18:31 -0500

    render-1

--- my-terminal.js
+++ my-terminal.js
@@ -5,26 +5,36 @@ figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
 figlet.preloadFonts([font], ready);
 
 const term = $('body').terminal(commands, {
-    greetings
+  greetings: false
 });
 
+term.pause();
+
 function ready() {
-  term.echo(greetings);
+ term.echo(() => render('Terminal Portfolio')).resume();
 }
 
-let term;
+function ready() {
+  term.echo(() => {
+    const ascii = render('Terminal Portfolio');
+    return `${ascii}\nWelcome to my Terminal Portfolio\n`;
+  });
+}
 
 function ready() {
-  term = $('body').terminal(commands, {
-    greetings
-  });
+  term.echo(() => render('Terminal Portfolio'));
 }
 
 function render(text) {
   const cols = term.cols();
-  return figlet.textSync(text, {
-    font: font,
-    width: cols,
-    whitespaceBreak: true
-  });
+  return trim(figlet.textSync(text, {
+      font: font,
+      width: cols,
+      whitespaceBreak: true
+  }));
 }
+
+function trim(str) {
+  return str.replace(/[\n\s]+$/, '');
+}
+
