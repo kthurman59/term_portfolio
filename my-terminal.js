705be805799a027891ddefa2f250124b1550882f
kevin <kthurman59@gmail.com>
2024-05-09 13:00:53 -0500

    render

--- my-terminal.js
+++ my-terminal.js
@@ -1,3 +1,30 @@
-const commands = {}
 
-const term = $('body').terminal(commands);
+const font = 'Slant';
+
+figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
+figlet.preloadFonts([font], ready);
+
+const term = $('body').terminal(commands, {
+    greetings
+});
+
+function ready() {
+  term.echo(greetings);
+}
+
+let term;
+
+function ready() {
+  term = $('body').terminal(commands, {
+    greetings
+  });
+}
+
+function render(text) {
+  const cols = term.cols();
+  return figlet.textSync(text, {
+    font: font,
+    width: cols,
+    whitespaceBreak: true
+  });
+}
